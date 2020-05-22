a5ef6c99bdace3b02005d341cb1308544ab1be4c
aziz anwar <azizanwar1705@gmail.com>
2020-05-22 19:53:47 +0700

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Router from './src/config/router';
+import { Provider } from 'react-redux';
+import { store } from './src/redux/index';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+const App = () => {
+  return(
+    <Provider store={store}>
+      <NavigationContainer>
+        <Router />
+      </NavigationContainer>
+    </Provider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
